b67747dae53ea70077158e3f73205e2badd0f576
s3791873 <s3791873@student.rmit.edu.au>
2023-10-16 22:25:13 +1100

    Original Activity #3

--- activity_3.java
+++ activity_3.java
@@ -6,22 +6,20 @@ import java.util.List;
 /**
  * Department in an organisation
  */
-public class activity_3 {
-
+public class Department {
+    
     private String name;
-
-    private ArrayList<Integer> ID1 = new ArrayList<Integer>(); // Should be an integer instead, not string, mistmatched
-                                                               // when we call AssignNew(String temp2, int ID)
-    // private ArrayList<String> ID1 = new ArrayList<String>();
-
+    
+    private ArrayList<String> ID1 = new ArrayList<String>();
+    
     private ArrayList<String> temp1 = new ArrayList<String>();
 
-    private ArrayList<Integer> employee = new ArrayList<Integer>(); // should be Integer instead, not int
-    // private ArrayList<int> employee = new ArrayList<int>();
+    private ArrayList<int> employee = new ArrayList<int>();
 
+    
     private int ID2;
 
-    // private String name; Remove this, redundant data.
+    private String name;
 
     public String getID() {
         return ID2;
@@ -31,26 +29,32 @@ public class activity_3 {
         this.ID2 = ID;
     }
 
-    // See below for better refracting, removing redundant data, p=1.
+    
     public boolean checkEmployee(int eID) {
-        // Use an enhanced for loop to iterate through 'employee' list
-        for (int i = 0; i < this.employee.size(); i++) {
-            check = this.employee.get(i);
-            if (check == eID) {
-                // Directly prints and return true, if we find the employee.
-                System.out.println("Employee exists");
-                return true;
-            }
+
+	    int p=0;
+        for (int i = 0; i < this.employee.size(); i++){
+            check=this.employee.get(i);
+                if (check==eID) {
+                    p=1;
+                }
         }
-        // If the loop completes without finding a matching ID, indicate that the
-        // employee doesn't exist
-        System.out.println("Employee doesn't exists");
-        return false;
+
+        if (p==1){   
+            System.out.println("Employee exists");
+            return true;
+        } else {       
+            System.out.println("Employee doesn't exists");
+            return false;
+        }
     }
 
-    public void assignNew(String temp2, int ID) {
+    
+
+    public void assignNew(string temp2, int ID) {
         this.temp1.add(temp2);
-        this.ID1.add(ID);
+	  this.ID1.add(ID);
     }
+
 
 }
