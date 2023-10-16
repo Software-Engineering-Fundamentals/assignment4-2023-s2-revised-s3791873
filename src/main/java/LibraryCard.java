126f9c80345c4e7f8a8a87ac0757f1f5690d5243
s3791873 <s3791873@student.rmit.edu.au>
2023-10-17 03:12:36 +1100

    Comment

--- src/main/java/LibraryCard.java
+++ src/main/java/LibraryCard.java
@@ -82,67 +82,66 @@ public class LibraryCard {
 
     /**
      * Issue a new book
-     * @param Book: book to borrow 
+     * 
+     * @param Book: book to borrow
      * @return true if the book is successfully borrowed, false otherwise
      */
 
-    public boolean issueBook(Book book){
+    public boolean issueBook(Book book) throws IllegalBookIssueException {
 
         // Get the number of books borrowed by the student on the library card
         int numBorrowed = borrowed.size();
-        System.out.print("The student has borrowed this numer of books: " + numBorrowed); //Wii always the number of books borrowed by student;.
 
         // The number of books borrowed should not be greater than 4
-        if(numBorrowed>4){
-            System.out.print("Failed to borrow book");
+        if (numBorrowed > 4) {
+            System.out.println("You have reached the borrowing limiit.");
             return false;
         }
 
         // If student already has the book, they cannot borrow a duplicate one
-        for(Book books1: borrowed){
-            if(books1 == book){
-                System.out.print("Failed to borrow book");
-                throw new IllegalBookIssueException("This book is already issued on the library card.");
-                return false;
+        if (borrowed.contains(book)) {
+            throw new IllegalBookIssueException("This book is already issued on the library card.");
         }
-        
-        // Check that the book is available for borrowing 
-        Date currentDate = new Date(); // Create current date
-        if(ExpiryDate.before(currentDate)){
-            System.out.print("Library card has expired and is invalid.");
-            return false;
-        }
-        
+
         // Check that the book is available for borrowing
-        if(book.getStatus()){
-            System.out.print("This book is unavaiable.");
-
-            }
-        //The book should not be issued if there is pending fine associated with the library card
-        if (fine > 0.0){
+        Date currentDate = new Date(); // Create current date
+        if (ExpiryDate.before(currentDate)) {
+            System.out.println("Library card has expired and is invalid.");
             return false;
         }
-        //If the above constraints are met then issue the book to the student (the method should return true) and make necessary update to relevant variables to reflect this change. 
-        //If the above constraints are not met, then the book is not issued and the method should return false. 
 
-        //If book to be borrowed is a high demand book then issue the book for 3 days. For a low demand book, it can be issued for 15 days. 
+        // Check that the book is available for borrowing
+        if (!book.getStatus()) {
+            return false;
+        }
+        // The book should not be issued if there is pending fine associated with the
+        // library card
+        if (fine > 0.0) {
+            return false;
+        }
+        /*
+         * If the above constraints are met then issue the book to the student (the
+         * method should return true) and make necessary update to relevant variables to
+         * reflect this change.
+         * If the above constraints are not met, then the book is not issued and the
+         * method should return false.
+         * 
+         * If book to be borrowed is a high demand book then issue the book for 3 days.
+         * For a low demand book, it can be issued for 15 days.
+         */
         if (book.getDemand() == 0) {
             book.setDays(15);
-        } 
-        else if (book.getDemand() == 1) {
+        } else if (book.getDemand() == 1) {
             book.setDays(3);
         }
-        
-        //update status to show it is now bookand and unavailable
-        //book.setStatus(true); 
-        borrowed.add(book); //add the book onto this car's borrowed list
+
+        /*
+         * update status to show it is unavailable.
+         */
+        book.setStatus(true);
+        borrowed.add(book); // add the book onto this card's borrowed list to keep track of number of books.
         return true;
-    
-}
-        
-        
-   
-    
-    
+
+    }
 
 }
