3d516ddeb7d1ff15d31b426653feb628cee1dc84
s3791873 <s3791873@student.rmit.edu.au>
2023-10-16 23:29:02 +1100

    Implemented Method

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -7,52 +7,42 @@ import java.util.List;
  * Book available in the library
  */
 public class Book {
-    
+
     /**
      * Book ID
      */
-    private int  ID;
-
+    private int ID;
 
     /**
      * Book title
      */
     private String title;
 
-
-	/**
+    /**
      * Authors
      */
     private ArrayList<String> authors = new ArrayList<String>();
-
-
 
     /**
      * Book status: true - Available; false - Not available
      */
     private boolean status = true;
 
-
     /**
      * Book demand: 0 - low demand; 1 - high demand
      */
     private int demand;
 
-   /**
+    /**
      * Return days of the book
      */
     private int days = 0;
 
-
-
-
     public Book(int ID, String title, int demand) {
         this.ID = ID;
         this.title = title;
-	   this.demand = demand;
+        this.demand = demand;
     }
-
-
 
     public int getDemand() {
         return this.demand;
@@ -69,18 +59,14 @@ public class Book {
     public void setID(int ID) {
         this.ID = ID;
     }
-    
-   
 
     public void setTitle(String title) {
         this.title = title;
     }
 
-
     public String getTitle() {
         return this.title;
     }
-
 
     public int days() {
         return days;
@@ -97,8 +83,5 @@ public class Book {
     public void setDemand(boolean status) {
         this.status = status;
     }
-
-
-
 
 }
--- src/main/java/LibraryCard.java
+++ src/main/java/LibraryCard.java
@@ -4,11 +4,11 @@ import java.util.Date;
 import java.util.List;
 
 /**
- * Library Card associated with the Student 
+ * Library Card associated with the Student
  */
 public class LibraryCard {
     /**
-     * Card id 
+     * Card id
      */
     private int ID;
 
@@ -33,20 +33,16 @@ public class LibraryCard {
     private double fine;
 
     /**
-     *  Details about the cardholder
+     * Details about the cardholder
      */
     private Student student;
-
-
-
 
     public LibraryCard(Student student, Date IssueDate, Date ExpiryDate, int ID) {
         this.student = student;
         this.IssueDate = IssueDate;
-	   this.ExpiryDate = ExpiryDate;
-	   this.ID = ID;
+        this.ExpiryDate = ExpiryDate;
+        this.ID = ID;
     }
-
 
     public int getID() {
         return this.ID;
@@ -64,7 +60,6 @@ public class LibraryCard {
         this.fine = fine;
     }
 
-
     public Date getIssueDate() {
         return IssueDate;
     }
@@ -81,12 +76,9 @@ public class LibraryCard {
         this.ExpiryDate = ExpiryDate;
     }
 
-    
     public List<Book> getBooks() {
         return borrowed;
     }
-
-    
 
     /**
      * Issue a new book
@@ -95,11 +87,62 @@ public class LibraryCard {
      */
 
     public boolean issueBook(Book book){
-    	return false;
+
+        // Get the number of books borrowed by the student on the library card
+        int numBorrowed = borrowed.size();
+        System.out.print("The student has borrowed this numer of books: " + numBorrowed); //Wii always the number of books borrowed by student;.
+
+        // The number of books borrowed should not be greater than 4
+        if(numBorrowed>4){
+            System.out.print("Failed to borrow book");
+            return false;
+        }
+
+        // If student already has the book, they cannot borrow a duplicate one
+        for(Book books1: borrowed){
+            if(books1 == book){
+                System.out.print("Failed to borrow book");
+                throw new IllegalBookIssueException("This book is already issued on the library card.");
+                return false;
+        }
+        
+        // Check that the book is available for borrowing 
+        Date currentDate = new Date(); // Create current date
+        if(ExpiryDate.before(currentDate)){
+            System.out.print("Library card has expired and is invalid.");
+            return false;
+        }
+        
+        // Check that the book is available for borrowing
+        if(book.getStatus()){
+            System.out.print("This book is unavaiable.");
+
+            }
+        //The book should not be issued if there is pending fine associated with the library card
+        if (fine > 0.0){
+            return false;
+        }
+        //If the above constraints are met then issue the book to the student (the method should return true) and make necessary update to relevant variables to reflect this change. 
+        //If the above constraints are not met, then the book is not issued and the method should return false. 
+
+        //If book to be borrowed is a high demand book then issue the book for 3 days. For a low demand book, it can be issued for 15 days. 
+        if (book.getDemand() == 0) {
+            book.setDays(15);
+        } 
+        else if (book.getDemand() == 1) {
+            book.setDays(3);
+        }
+        
+        //update status to show it is now bookand and unavailable
+        //book.setStatus(true); 
+        borrowed.add(book); //add the book onto this car's borrowed list
+        return true;
+    
+}
+        
+        
    
-    }
-
-
-
+    
+    
 
 }
